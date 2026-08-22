0eed2d04279917e1593b4d3ace396cb6c31c594b
JayanthBalan <jayanthsb.005@gmail.com>
2026-08-22 10:12:40 +0000

    Finished ass5p1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -31,13 +31,12 @@ int main(int argc, char *argv[])
     int server_fd = -1;
     int daemon_mode = 0;
 
-    struct addrinfo hints;
+    struct addrinfo hints = {0};
     struct addrinfo *servinfo = NULL;
     struct addrinfo *iter = NULL;
 
     // CLA
     if (argc > 2) {
-        fprintf(stderr, "Usage: %s [-d]\n", argv[0]);
         return -1;
     }
 
@@ -46,7 +45,6 @@ int main(int argc, char *argv[])
             daemon_mode = 1;
         }
         else {
-            fprintf(stderr, "Usage: %s [-d]\n", argv[0]);
             return -1;
         }
     }
@@ -54,31 +52,23 @@ int main(int argc, char *argv[])
     openlog("aesdsocket", LOG_PID | LOG_CONS, LOG_USER);
 
     // Signal Handling
-    struct sigaction sa;
-
-    memset(&sa, 0, sizeof(sa));
-
+    struct sigaction sa = {0};
     sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
-
     sa.sa_flags = 0;
 
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
+    if (sigaction(SIGINT, &sa, NULL) != 0) {
         syslog(LOG_ERR, "sigaction(SIGINT) failed: %s", strerror(errno));
         closelog();
         return -1;
     }
-
-    if (sigaction(SIGTERM, &sa, NULL) == -1) {
+    if (sigaction(SIGTERM, &sa, NULL) != 0) {
         syslog(LOG_ERR, "sigaction(SIGTERM) failed: %s", strerror(errno));
         closelog();
         return -1;
     }
 
-
     // Server Address Configuration
-    memset(&hints, 0, sizeof(hints));
-
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
@@ -87,38 +77,27 @@ int main(int argc, char *argv[])
 
     // Local Address Info
     int status = getaddrinfo(NULL, PORT, &hints, &servinfo);
-
     if (status != 0) {
         syslog(LOG_ERR, "getaddrinfo: %s", gai_strerror(status));
         closelog();
         return -1;
     }
 
-
     // Address Iteration
     for (iter = servinfo; iter != NULL; iter = iter->ai_next) {
-
-        server_fd =
-            socket(iter->ai_family, iter->ai_socktype, iter->ai_protocol);
-
+        server_fd = socket(iter->ai_family, iter->ai_socktype, iter->ai_protocol);
         if (server_fd == -1) {
             continue;
         }
 
         int opt = 1;
         if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-
             syslog(LOG_ERR, "setsockopt failed: %s", strerror(errno));
-
             close(server_fd);
             server_fd = -1;
             continue;
         }
 
-
-        /*
-         * Bind to port 9000.
-         */
         if (bind(server_fd, iter->ai_addr, iter->ai_addrlen) == 0) {
             break;
         }
@@ -126,22 +105,16 @@ int main(int argc, char *argv[])
         close(server_fd);
         server_fd = -1;
     }
-
     if (iter == NULL || server_fd == -1) {
-
-        syslog(LOG_ERR,
-               "Failed to bind socket");
-
+        syslog(LOG_ERR, "Failed to bind socket");
         freeaddrinfo(servinfo);
         closelog();
-
         return -1;
     }
 
     // Daemon vs Process
     if (daemon_mode) {
         pid_t pid = fork();
-
         if (pid < 0) {
             syslog(LOG_ERR, "fork failed: %s", strerror(errno));
             close(server_fd);
@@ -177,7 +150,6 @@ int main(int argc, char *argv[])
         dup2(devnull, STDIN_FILENO);
         dup2(devnull, STDOUT_FILENO);
         dup2(devnull, STDERR_FILENO);
-
         if (devnull > STDERR_FILENO) {
             close(devnull);
         }
@@ -194,17 +166,12 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-
     // Infinity
     while (!exit_requested) {
-
         struct sockaddr_storage client_addr;
-        socklen_t client_addr_len =
-            sizeof(client_addr);
+        socklen_t client_addr_len = sizeof(client_addr);
 
-        int client_fd =
-            accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
         if (client_fd == -1) {
             if (errno == EINTR) {
                 if (exit_requested) {
@@ -212,28 +179,23 @@ int main(int argc, char *argv[])
                 }
                 continue;
             }
-
             syslog(LOG_ERR, "accept failed: %s", strerror(errno));
             break;
         }
 
         // Client IP
         char client_ip[INET6_ADDRSTRLEN];
-
         if (get_client_ip(&client_addr, client_ip, sizeof(client_ip)) == -1) {
             strncpy(client_ip, "unknown", sizeof(client_ip));
             client_ip[sizeof(client_ip) - 1] = '\0';
         }
 
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
-
         handle_client(client_fd);
-
 
         close(client_fd);
         syslog(LOG_INFO, "Closed connection from %s", client_ip);
     }
-
 
     syslog(LOG_INFO, "Caught signal, exiting");
     close(server_fd);
@@ -258,22 +220,17 @@ static int handle_client(int client_fd)
 
     while (!exit_requested) {
         ssize_t bytes_received = recv(client_fd, recv_buffer, sizeof(recv_buffer), 0);
-
         if (bytes_received < 0) {
-
             if (errno == EINTR) {
                 if (exit_requested) {
                     break;
                 }
-
                 continue;
             }
-
             syslog(LOG_ERR, "recv failed: %s", strerror(errno));
             free(packet_buffer);
             return -1;
         }
-
         if (bytes_received == 0) {
             break;
         }
@@ -281,7 +238,6 @@ static int handle_client(int client_fd)
         for (ssize_t i = 0; i < bytes_received; i++) {
             if (packet_length + 1 > packet_capacity) {
                 size_t new_capacity;
-
                 if (packet_capacity == 0) {
                     new_capacity = 1024;
                 }
@@ -290,10 +246,8 @@ static int handle_client(int client_fd)
                 }
 
                 char *new_buffer = realloc(packet_buffer, new_capacity);
-
                 if (new_buffer == NULL) {
                     syslog(LOG_ERR, "malloc/realloc failed while receiving packet");
-
                     free(packet_buffer);
                     return -1;
                 }
@@ -307,24 +261,18 @@ static int handle_client(int client_fd)
 
             if (recv_buffer[i] == '\n') {
                 if (append_packet(packet_buffer, packet_length) == -1) {
-
                     free(packet_buffer);
                     return -1;
                 }
-
                 if (send_file_to_client(client_fd) == -1) {
-
                     free(packet_buffer);
                     return -1;
                 }
-
                 packet_length = 0;
             }
         }
     }
-
     free(packet_buffer);
-
     return 0;
 }
 
@@ -332,14 +280,12 @@ static int get_client_ip(struct sockaddr_storage *client_addr, char *ip_buffer, 
 {
     void *address;
 
-    if (client_addr->ss_family == AF_INET) {
+    if (client_addr->ss_family == PF_INET) {
         struct sockaddr_in *ipv4 = (struct sockaddr_in *)client_addr;
-
         address = &(ipv4->sin_addr);
     }
-    else if (client_addr->ss_family == AF_INET6) {
+    else if (client_addr->ss_family == PF_INET6) {
         struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)client_addr;
-
         address = &(ipv6->sin6_addr);
     }
     else {
@@ -355,10 +301,7 @@ static int get_client_ip(struct sockaddr_storage *client_addr, char *ip_buffer, 
 
 static int append_packet(const char *packet, size_t packet_length)
 {
-    int file_fd;
-
-    file_fd = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
-
+    int file_fd = open(DATA_FILE, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (file_fd == -1) {
         syslog(LOG_ERR, "Failed to open %s for writing: %s", DATA_FILE, strerror(errno));
         return -1;
@@ -367,23 +310,18 @@ static int append_packet(const char *packet, size_t packet_length)
     size_t total_written = 0;
     while (total_written < packet_length) {
         ssize_t written = write(file_fd, packet + total_written, packet_length - total_written);
-
         if (written < 0) {
             if (errno == EINTR) {
                 continue;
             }
-
             syslog(LOG_ERR, "Failed to write %s: %s", DATA_FILE, strerror(errno));
-
             close(file_fd);
             return -1;
         }
-
         total_written += written;
     }
 
     close(file_fd);
-
     return 0;
 }
 
@@ -401,31 +339,27 @@ static int send_file_to_client(int client_fd)
 
     while (1) {
         ssize_t bytes_read = read(file_fd, buffer, sizeof(buffer));
-
         if (bytes_read < 0) {
             if (errno == EINTR) {
                 continue;
             }
-
             syslog(LOG_ERR, "Failed to read %s: %s", DATA_FILE, strerror(errno));
-
             close(file_fd);
             return -1;
         }
 
-        if (bytes_read == 0)
+        if (bytes_read == 0) {
             break;
+        }
 
         if (send_all(client_fd, buffer, bytes_read) == -1) {
             syslog(LOG_ERR, "Failed to send file to client: %s", strerror(errno));
-
             close(file_fd);
             return -1;
         }
     }
 
     close(file_fd);
-
     return 0;
 }
 
@@ -441,24 +375,21 @@ static void signal_handler(int signo)
 static int send_all(int fd, const char *buffer, size_t length)
 {
     size_t total_sent = 0;
-
     while (total_sent < length) {
         ssize_t sent = send(fd, buffer + total_sent, length - total_sent, 0);
-
         if (sent < 0) {
             if (errno == EINTR) {
                 if (exit_requested) {
                     return -1;
                 }
-
                 continue;
             }
-
             return -1;
         }
 
-        if (sent == 0)
+        if (sent == 0) {
             return -1;
+        }
 
         total_sent += sent;
     }
